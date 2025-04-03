500fd0b8aa4906943d73dd26302990d5d83ca6b6
SplinteeX <Jepsteri04@gmail.com>
2025-04-03 14:28:13 +0300

    Test github webhook

--- app/src/main/java/com/example/hardwarecheck/screens/HardwareScreen.kt
+++ app/src/main/java/com/example/hardwarecheck/screens/HardwareScreen.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hardwarecheck.model.DeviceInfo
 import com.example.hardwarecheck.R
-
+// --
 @Composable
 fun HardwareScreen(deviceInfo: DeviceInfo) {
     Column(

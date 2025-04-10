75cd0cbda32b7b3607112dc5b48c0196d43bb956
Peetu <peetu.rasanen@metropolia.fi>
2025-04-10 13:59:12 +0300

    Update Firebase integration to collect and save device info dynamically

--- app/src/main/java/com/example/hardwarecheck/MainActivity.kt
+++ app/src/main/java/com/example/hardwarecheck/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hardwarecheck
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -16,6 +17,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.hardwarecheck.database.FirestoreManager
 import com.example.hardwarecheck.navigation.BottomNavigationBar
 import com.example.hardwarecheck.navigation.Screen
 import com.example.hardwarecheck.screens.HardwareScreen
@@ -23,39 +25,22 @@ import com.example.hardwarecheck.screens.OverviewScreen
 import com.example.hardwarecheck.screens.ProfileScreen
 import com.example.hardwarecheck.ui.theme.HardwareCheckTheme
 import com.example.hardwarecheck.utils.HardwareInfoUtils
-
-import com.example.hardwarecheck.database.FirestoreManager
-import com.example.hardwarecheck.model.DeviceInfo
 import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Initialize Firebase
         FirebaseApp.initializeApp(this)
 
         val firestoreManager = FirestoreManager()
 
-        // Example device ID
-        val deviceId = "testDevice123456"
+        val deviceInfo = HardwareInfoUtils.collectDeviceInfo(this)
+        val deviceId = android.provider.Settings.Secure.getString(contentResolver, android.provider.Settings.Secure.ANDROID_ID)
 
-        // Example DeviceInfo object
-        val deviceInfo = DeviceInfo(
-            model = "Pixel 7",
-            osInfo = "Android 14",
-            processor = "Google Tensor",
-            memory = "8GB",
-            cores = "8",
-            storage = "128GB",
-            gpu = "Mali-G78",
-            sensors = "Accelerometer, Gyroscope"
-        )
-
-        // Save the device info to Firestore
         firestoreManager.saveDeviceInfo(deviceId, deviceInfo)
 
-        // Retrieve the device info from Firestore
         firestoreManager.getDeviceInfo(deviceId) { retrievedDeviceInfo ->
             if (retrievedDeviceInfo != null) {
                 Log.d("FirestoreTest", "Retrieved Device Info: $retrievedDeviceInfo")
